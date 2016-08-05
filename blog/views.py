e416aadd9bb25b792c11d2f0c67ba5f55125c8f2
zhanglei <190128084@qq.com>
2016-08-05 10:47:03 +0800

    增加文章编辑页

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,81 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import redirect
+
 # Create your views here.
 
 # def post_list(request):
 # 	return render(request,'blog/post_list.html',{})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    """所有已发布文章"""
+    postsAll = Post.objects.annotate(num_comment=Count('comment')).filter(published_date__isnull=False).prefetch_related(
+        'category').prefetch_related('tags').order_by('-published_date')
+    for p in postsAll:
+        p.click = cache_manager.get_click(p)
+    paginator = Paginator(postsAll, 10)  # Show 10 contacts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'page': True})
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def test(request):
+    posts = Post.objects.filter(published_date__isnull=False)
+    return render(request,'blog/test.html',{'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
